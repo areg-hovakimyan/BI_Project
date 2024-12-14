e3b5cf20a460f40d11d065771c90399739efeae9
LusineToro <104370727+LusineToro@users.noreply.github.com>
2024-12-14 15:43:35 +0400

    Update update_dim_Categories.sql

---
+++ pipeline_dimensional_data/queries/update_dim_Categories.sql
@@ -0,0 +1,23 @@
+DECLARE @DimTable NVARCHAR(255) = 'DimCategories';
+DECLARE @StagingTable NVARCHAR(255) = 'Staging_Categories';
+DECLARE @KeyColumn NVARCHAR(255) = 'CategoryID';
+DECLARE @Columns NVARCHAR(MAX) = 'CategoryName, Description';
+
+DECLARE @SQL NVARCHAR(MAX);
+
+SET @SQL = '
+MERGE dbo.' + @DimTable + ' AS DST 
+USING dbo.' + @StagingTable + ' AS SRC
+ON (SRC.' + @KeyColumn + ' = DST.' + @KeyColumn + ')
+WHEN NOT MATCHED THEN
+    INSERT (' + @KeyColumn + ', ' + @Columns + ')
+    VALUES (SRC.' + @KeyColumn + ', SRC.CategoryName, SRC.Description)
+WHEN MATCHED AND (ISNULL(DST.CategoryName, '''') <> ISNULL(SRC.CategoryName, '''')
+                   OR ISNULL(DST.Description, '''') <> ISNULL(SRC.Description, ''''))
+    THEN
+        UPDATE SET
+            DST.CategoryName = SRC.CategoryName,
+            DST.Description = SRC.Description;';
+
+PRINT @SQL;
+EXEC sp_executesql @SQL;
